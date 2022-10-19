8c03bd2b1c2357c7da2574d456c304bf02f4e674
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2022-10-19 11:24:04 +0200

    [FEAT] Add Premake

---
+++ premake5.lua
@@ -0,0 +1,99 @@
+workspace "NegativeEngine"
+	architecture "x64"
+	startproject "SandBox"
+	configurations {"Debug", "Release", "Dist"}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "NegativeEngine"
+	location "NegativeEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		
+	}
+
+	filter "system:windows"
+		cppdialect "C++latest"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS",
+			"NE_BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandBox")
+		}
+
+	filter "configurations:Debug"
+		defines "NE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NE_DIST"
+		optimize "On"
+
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"NegativeEngine/src"
+	}
+
+	links 
+	{
+		"NegativeEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++latest"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "NE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NE_DIST"
+		optimize "On"
